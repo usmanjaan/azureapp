b6dd4fe44c0a64d23e700039d8f2b477a2d4ce5b
usmanjaan <usmanshahid@live.ie>
2018-12-03 22:00:13 +0000

    3.3.2

--- azureapp/src/main/java/ie/usmancom/MyUI.java
+++ azureapp/src/main/java/ie/usmancom/MyUI.java
@@ -45,7 +45,7 @@ public class MyUI extends UI {
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
-        Button button = new Button("Please");
+        Button button = new Button("Change");
         button.addClickListener(e -> {
             layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
         });
